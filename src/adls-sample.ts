ebf68537069e5aa26881a05ebd79e9d9eeced333
davidjen@DAVIDJENDEV <david@davidjenni.com>
2016-10-26 16:09:29 -0700

    enumerate dirs and files

--- src/adls-sample.ts
+++ src/adls-sample.ts
@@ -1,9 +1,21 @@
-import {interactiveLogin, InteractiveLoginOptions} from 'ms-rest-azure';
-import {} from "azure-arm-datalake-store";
+import {interactiveLogin, InteractiveLoginOptions, UserTokenCredentials} from 'ms-rest-azure';
+import {TokenCredentials} from 'ms-rest';
+// import {DataLakeStoreFileSystemClient} from 'azure-arm-datalake-store';
+let adls = require('azure-arm-datalake-store');
 
 let options: InteractiveLoginOptions = {
 };
 
 let accountName = 'dprepdemostore';
 interactiveLogin(options, (err, credentials) => {
+    let fileSystemClient = new adls.DataLakeStoreFileSystemClient(credentials);
+    fileSystemClient.fileSystem.listFileStatus(accountName, '/opendata/swiss/SBB', (error, result) => {
+        if (error) {
+            console.log(err);
+        }
+        // sigh, the plural of status is stati...
+        result.fileStatuses.fileStatus.forEach((file) => {
+            console.log(`type: ${file.type}, name: ${file.pathSuffix}`);
+        });
+    });
 });
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,4 @@
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="../node_modules/azure-arm-datalake-store/lib/filesystem/dataLakeStoreFileSystemManagementClient.d.ts" />
+/// <reference path="../node_modules/azure-arm-datalake-store/lib/filesystem/models/index.d.ts" />
